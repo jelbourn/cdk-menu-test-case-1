77423a62e84d5116a47b071842d56fff557a2531
Jeremy Elbourn <jelbourn@gmail.com>
2020-08-12 13:53:30 -0700

    menu party

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CdkMenuModule} from '@angular/cdk-experimental/menu';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CdkMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
